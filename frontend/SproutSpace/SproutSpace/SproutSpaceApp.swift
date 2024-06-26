8acf604d90a19a4b44c1643493ed225d9ae75b2e
Nathan Tieu <ntieu3@gatech.edu>
2024-06-26 11:07:52 -0400

    initial commit

---
+++ frontend/SproutSpace/SproutSpace/SproutSpaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SproutSpaceApp.swift
+//  SproutSpace
+//
+//  Created by nathan tieu on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SproutSpaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
