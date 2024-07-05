129cb596b328a8719c342172a32ad248089a3cb6
Nathan Tieu <ntieu3@gatech.edu>
2024-07-04 20:06:39 -0400

    attempt 5 to connect api

--- frontend/SproutSpace/SproutSpace/SproutSpaceApp.swift
+++ frontend/SproutSpace/SproutSpace/SproutSpaceApp.swift
@@ -5,24 +5,11 @@
 //  Created by nathan tieu on 6/26/24.
 //
 
+import Foundation
 import SwiftUI
-
-var apiKey: String = ""
 
 @main
 struct SproutSpaceApp: App {
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        if let apiKeyFromEnv = ProcessInfo.processInfo.environment["API_KEY"] {
-            apiKey = apiKeyFromEnv
-            print(apiKey)
-        } else {
-            print("API_KEY environment variable is not set.")
-        }
-        return true
-    }
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
